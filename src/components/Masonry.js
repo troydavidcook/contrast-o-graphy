440a771c1305eecc41e7ab168a27aca449f3fb51
Troy David Cook <www.troydavidcook@gmail.com>
2018-02-26 14:09:20 -0700

    init commit, added first two components

---
+++ src/components/Masonry.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Masonry extends React.Component {
+    render () {
+        return (
+            <div className="img-body">
+             <div className="container">
+            </div>
+          </div>
+        );
+    }
+}
+
+
+export default Masonry;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <nav className="navbar navbar-inverse navbar-fixed-top">
+                <h6 className="navbar-brand">Contrast<span className="yellow-text">-O-</span>graphy</h6>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
